100e487d061b71dcfd50c8d7bc22890dd2d3cbb6
Reyshawn <reshawnchang@gmail.com>
2019-06-18 16:45:38 +0800

    Dashboard simple layout

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,17 +1,40 @@
 <template>
-  <div>
-    <p>
-      This is a dashboard
-    </p>
+  <div class="dashboard-container">
+    <div class="box box1"></div>
+    <div class="box box2"></div>
+    <div class="box box3"></div>
+    <div class="box box4"></div>
+    <div class="box box5"></div>
+    <div class="box box6"></div>
   </div>
 </template>
 
 <script>
-export default {
 
+export default {
+  name: 'dashboard'
 }
 </script>
 
-<style>
+<style scoped>
+.dashboard-container {
+  margin: 2em;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-auto-rows: minmax(240px, auto);
+  grid-gap: 1em;
+}
+
+.box1 {
+  grid-row: 1/4;
+}
+
+.box2, .box3 {
+  grid-column: 2/5;
+}
+
+.box:not(:last-child) {
+  margin: 0;
+}
 
 </style>
